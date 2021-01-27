1f2bc18a01002155ed59f4b4dec8b55978e5d17c
mateotis <hekfuszmate@gmail.com>
2021-01-27 17:57:01 +0400

    Added exporting scraped data to csv file, also tested with all four academic divisions.

--- facultyEduScraper.py
+++ facultyEduScraper.py
@@ -3,13 +3,14 @@ import requests
 import urllib
 import csv
 
-# Science
-url = "https://nyuad.nyu.edu/en/academics/divisions/science/faculty.html"
+url = "https://nyuad.nyu.edu/en/academics/divisions/arts-and-humanities/faculty.html"
 response = requests.get(url)
 
 soup = bs(response.text,"html.parser")
 
-for i in range(0,100): # Arbitrary range, faculty numbers are much less than this, ensuring the program goes over everyone
+facultyTotal = [] # All faculty in the division
+
+for i in range(0,200): # Arbitrary range, faculty numbers are much less than this, ensuring the program goes over everyone
 
 	facultyInfo = [] # List to store all collected data points
 
@@ -20,7 +21,7 @@ for i in range(0,100): # Arbitrary range, faculty numbers are much less than thi
 
 	facultyName = faculty.find("a").text # Their name is within a link
 	facultyInfo.append(facultyName)
-	facultyInfo.append("Science")
+	facultyInfo.append("Arts and Humanities")
 	facultyEduLoc = faculty.text.find("Education") # Find the Education string
 
 	if(facultyEduLoc == -1):
@@ -56,5 +57,16 @@ for i in range(0,100): # Arbitrary range, faculty numbers are much less than thi
 			facultyOther.append(almamater)
 
 	facultyInfo.extend([facultyBachelors, facultyMasters, facultyDoctorates, facultyOther])
+	facultyTotal.append(facultyInfo)
 
-	print("Name:", facultyInfo[0], "\n", "Division:", facultyInfo[1], "\n", "Bachelor's:", facultyInfo[2], "\n", "Master's:", facultyInfo[3], "\n", "Doctorate:", facultyInfo[4], "\n", "Other:", facultyInfo[5])
+	#print("Name:", facultyInfo[0], "\n", "Division:", facultyInfo[1], "\n", "Bachelor's:", facultyInfo[2], "\n", "Master's:", facultyInfo[3], "\n", "Doctorate:", facultyInfo[4], "\n", "Other:", facultyInfo[5])
+
+for facultyInfo in facultyTotal:
+	print("Name:", facultyInfo[0], "\n", "Division:", facultyInfo[1], "\n", "Bachelor's:", facultyInfo[2], "\n", "Master's:", facultyInfo[3], "\n", "Doctorate:", facultyInfo[4], "\n", "Other:", facultyInfo[5], "\n\n")
+
+with open('facultyEduInfo.csv', mode='w', encoding = "UTF-8") as fEduInfo:
+	fieldnames = ["Name", "Division", "Bachelor's", "Master's", "Doctorate", "Other"]
+	writer = csv.DictWriter(fEduInfo, fieldnames=fieldnames)
+	writer.writeheader()
+	for f in facultyTotal:
+		writer.writerow({'Name': f[0], 'Division': f[1], "Bachelor's": f[2], "Master's": f[3], "Doctorate": f[4], "Other": f[5]})
